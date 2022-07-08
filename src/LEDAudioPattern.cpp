3b36e57361fb12fc71a7a94d633bc86dc24e576f
Koji Gardiner <koji.gardiner@gmail.com>
2022-07-07 22:10:52 -0700

    Update comments and private member naming

--- src/LEDAudioPattern.cpp
+++ src/LEDAudioPattern.cpp
@@ -2,47 +2,47 @@
 
 #include "LEDPanel.h"
 
-// constructor
+// Constructor for base class, called by all subclasses
 LEDAudioPattern::LEDAudioPattern(LEDPanel *lp) {
-    this->lp = lp;
+    this->_lp = lp;  // set pointer to LED panel that will be updated
 }
 
 LEDAudioPattern::~LEDAudioPattern() {
 }
 
-// Initialize static variables
-uint8_t LEDNoisePattern::ihue = 0;
-uint16_t LEDNoisePattern::x = random16();
-uint16_t LEDNoisePattern::y = random16();
-uint16_t LEDNoisePattern::z = random16();
-uint16_t LEDNoisePattern::target_x = x;
-uint16_t LEDNoisePattern::target_y = y;
-uint16_t LEDNoisePattern::speed = 1;
-uint16_t LEDNoisePattern::scale = 40;
-uint16_t LEDNoisePattern::target_scale = scale;
-uint8_t LEDNoisePattern::noise[GRID_H][GRID_W] = {0};
+// Initialize static variable used to maintain consistency of noise pattern across mode changes
+uint8_t LEDNoisePattern::_ihue = 0;
+uint16_t LEDNoisePattern::_x = random16();
+uint16_t LEDNoisePattern::_y = random16();
+uint16_t LEDNoisePattern::_z = random16();
+uint16_t LEDNoisePattern::_target_x = _x;
+uint16_t LEDNoisePattern::_target_y = _y;
+uint16_t LEDNoisePattern::_speed = 1;
+uint16_t LEDNoisePattern::_scale = 40;
+uint16_t LEDNoisePattern::_target_scale = _scale;
+uint8_t LEDNoisePattern::_noise[GRID_H][GRID_W] = {0};
 
-// Fill the x/y array of 8-bit noise values using the inoise8 function.
+// Fill the x/y array of 8-bit noise values using the inoise8 function. From FastLED.
 void LEDNoisePattern::_fill_noise8() {
     // If we're runing at a low "speed", some 8-bit artifacts become visible
     // from frame-to-frame.  In order to reduce this, we can do some fast data-smoothing.
     // The amount of data smoothing we're doing depends on "speed".
     uint8_t dataSmoothing = 0;
 
-    scale = int(round((target_scale * 0.3) + (scale * 0.7)));  // slowly move toward the new target_scale
-    x = int(round((target_x * 0.3) + (x * 0.7)));              // slowly move toward the new target_x
-    y = int(round((target_y * 0.3) + (y * 0.7)));              // slowly move toward the new target_y
+    _scale = int(round((_target_scale * 0.3) + (_scale * 0.7)));  // slowly move toward the new target_scale
+    _x = int(round((_target_x * 0.3) + (_x * 0.7)));              // slowly move toward the new target_x
+    _y = int(round((_target_y * 0.3) + (_y * 0.7)));              // slowly move toward the new target_y
 
-    if (speed < 50) {
-        dataSmoothing = 200 - (speed * 4);
+    if (_speed < 50) {
+        dataSmoothing = 200 - (_speed * 4);
     }
 
     for (int i = 0; i < GRID_W; i++) {
-        int ioffset = scale * (i - int(GRID_W / 2));  // center the scale shift
+        int ioffset = _scale * (i - int(GRID_W / 2));  // center the scale shift
         for (int j = 0; j < GRID_H; j++) {
-            int joffset = scale * (j - int(GRID_H / 2));  // center the scale shift
+            int joffset = _scale * (j - int(GRID_H / 2));  // center the scale shift
 
-            uint8_t data = inoise8(x + ioffset, y + joffset, z);
+            uint8_t data = inoise8(_x + ioffset, _y + joffset, _z);
 
             // The range of the inoise8 function is roughly 16-238.
             // These two operations expand those values out to roughly 0..255
@@ -51,22 +51,23 @@ void LEDNoisePattern::_fill_noise8() {
             data = qadd8(data, scale8(data, 39));
 
             if (dataSmoothing) {
-                uint8_t olddata = noise[i][j];
+                uint8_t olddata = _noise[i][j];
                 uint8_t newdata = scale8(olddata, dataSmoothing) + scale8(data, 256 - dataSmoothing);
                 data = newdata;
             }
 
-            noise[i][j] = data;
+            _noise[i][j] = data;
         }
     }
 
-    z += speed;
+    _z += _speed;
 
     // apply slow drift to X and Y, just for visual variation.
     // x += speed / 8;
     // y -= speed / 16;
 }
 
+// Sets LEDs based on noise grid and current palette. From FastLED.
 void LEDNoisePattern::_map_noise_to_leds_using_palette() {
     for (int i = 0; i < GRID_W; i++) {
         for (int j = 0; j < GRID_H; j++) {
@@ -74,8 +75,8 @@ void LEDNoisePattern::_map_noise_to_leds_using_palette() {
             // array for our brightness, and the flipped value from (j,i)
             // for our pixel's index into the color palette.
 
-            uint8_t index = noise[j][i];
-            uint8_t bri = noise[i][j];
+            uint8_t index = _noise[j][i];
+            uint8_t bri = _noise[i][j];
 
             // // if this palette is a 'loop', add a slowly-changing base value
             // if( colorLoop) {
@@ -90,107 +91,82 @@ void LEDNoisePattern::_map_noise_to_leds_using_palette() {
                 bri = dim8_raw(bri * 2);
             }
 
-            CRGB color = ColorFromPalette(lp->curr_palette, index, bri);
-            lp->set_xy(i, j, color);
+            CRGB color = ColorFromPalette(_lp->curr_palette, index, bri);
+            _lp->set_xy(i, j, color);
         }
     }
 
-    ihue += 1;
+    _ihue += 1;
 }
 
+// Generates a simplex noise pattern of LEDs. Based on FastLED implementation.
 void LEDNoisePattern::set_leds(int *intensity) {
-    // if (ap->beat_detected) {
-    //     uint8_t change_type = random8(6);  // select a random behavior
-
-    //     switch (change_type) {  // either change the scale or scan in XY
-    //         case 0:
-    //             target_scale = constrain(scale + random8(30, 40), 20, 50);
-    //             break;
-    //         case 1:
-    //             target_scale = constrain(scale - random8(30, 40), 20, 50);
-    //             break;
-    //         case 2:
-    //             target_x = constrain(x + 16 * (random8(3, 7)) * scale / 10, 0, 65535);  // scale by "scale" so that the moves look similar
-    //             break;
-    //         case 3:
-    //             target_y = constrain(y + 16 * (random8(3, 7)) * scale / 10, 0, 65535);
-    //             break;
-    //         case 4:
-    //             target_x = constrain(x - 16 * (random8(3, 7)) * scale / 10, 0, 65535);
-    //             break;
-    //         case 5:
-    //             target_y = constrain(y - 16 * (random8(3, 7)) * scale / 10, 0, 65535);
-    //             break;
-    //     }
-    // }
-
-    // generate noise data
     _fill_noise8();
-
-    // convert the noise data to colors in the LED array
-    // using the current palette
     _map_noise_to_leds_using_palette();
 }
 
+// Generates vertical bar pattern, with peaks that decay over time. Based on ESP32 FFT VU code.
 void LEDBarsPattern::set_leds(int *intensity) {
     for (int bar_x = 0; bar_x < GRID_W; bar_x++) {
         int max_y = int(round((float(intensity[bar_x]) / 255) * GRID_H));  // scale the intensity by the grid height
         max_y = constrain(max_y, 0, GRID_H - 1);
 
         // Move peak up
-        if (max_y > peaks[bar_x]) {
-            peaks[bar_x] = max_y;
+        if (max_y > _peaks[bar_x]) {
+            _peaks[bar_x] = max_y;
         }
 
         // Light up or darken each grid element
         for (int bar_y = 0; bar_y < GRID_H; bar_y++) {
             if (bar_y < max_y) {
                 int color_index = int(round(float(bar_x) / GRID_W * 255));
-                CRGB color = ColorFromPalette(lp->curr_palette, color_index, 255, lp->curr_blending);
-                lp->set_xy(bar_x, bar_y, color);
+                CRGB color = ColorFromPalette(_lp->curr_palette, color_index, 255, _lp->curr_blending);
+                _lp->set_xy(bar_x, bar_y, color);
             } else {
-                lp->set_xy(bar_x, bar_y, CRGB::Black);
+                _lp->set_xy(bar_x, bar_y, CRGB::Black);
             }
         }
-        lp->set_xy(bar_x, peaks[bar_x], CRGB::White);  // light up the peak
+        _lp->set_xy(bar_x, _peaks[bar_x], CRGB::White);  // light up the peak
 
-        if (counter % PEAK_DECAY_RATE == 0) {  // every X frames, shift peak down
-            if (peaks[bar_x] > 0) {
-                peaks[bar_x] -= 1;
+        if (_counter % PEAK_DECAY_RATE == 0) {  // every X frames, shift peak down
+            if (_peaks[bar_x] > 0) {
+                _peaks[bar_x] -= 1;
             }
         }
     }
-    counter++;
+    _counter++;
 }
 
+// Generates vertical peaks that decay and change color over time. Based on ESP32 FFT VU code.
 void LEDOutrunBarsPattern::set_leds(int *intensity) {
     for (int bar_x = 0; bar_x < GRID_W; bar_x++) {
         int max_y = int(round((float(intensity[bar_x]) / 255) * GRID_H));  // scale the intensity by the grid height
         max_y = constrain(max_y, 0, GRID_H - 1);
 
         // Move peak up
-        if (max_y > peaks[bar_x]) {
-            peaks[bar_x] = max_y;
+        if (max_y > _peaks[bar_x]) {
+            _peaks[bar_x] = max_y;
         }
 
         // Only light up the peak
         for (int bar_y = 0; bar_y < GRID_H; bar_y++) {
-            lp->set_xy(bar_x, bar_y, CRGB::Black);
+            _lp->set_xy(bar_x, bar_y, CRGB::Black);
         }
-        int color_index = int(round(float(peaks[bar_x]) / GRID_H * 255));
-        CRGB color = ColorFromPalette(lp->curr_palette, color_index, 255, lp->curr_blending);
+        int color_index = int(round(float(_peaks[bar_x]) / GRID_H * 255));
+        CRGB color = ColorFromPalette(_lp->curr_palette, color_index, 255, _lp->curr_blending);
 
-        lp->set_xy(bar_x, peaks[bar_x], color);  // light up the peak
+        _lp->set_xy(bar_x, _peaks[bar_x], color);  // light up the peak
 
-        if (counter % PEAK_DECAY_RATE == 0) {  // every X frames, shift peak down
-            if (peaks[bar_x] > 0) {
-                peaks[bar_x] -= 1;
+        if (_counter % PEAK_DECAY_RATE == 0) {  // every X frames, shift peak down
+            if (_peaks[bar_x] > 0) {
+                _peaks[bar_x] -= 1;
             }
         }
     }
-    counter++;
+    _counter++;
 }
 
+// Generates centered symmetric vertical bar pattern. Based on ESP32 FFT VU code.
 void LEDCenterBarsPattern::set_leds(int *intensity) {
     for (int bar_x = 0; bar_x < GRID_W; bar_x++) {
         int max_y = int(round((float(intensity[bar_x]) / 255) * GRID_H));  // scale the intensity by the grid height
@@ -202,34 +178,36 @@ void LEDCenterBarsPattern::set_leds(int *intensity) {
         for (int bar_y = 0; bar_y < GRID_H; bar_y++) {
             if (bar_y >= y_start && bar_y <= (y_start + max_y)) {
                 int color_index = constrain((bar_y - y_start) * (255 / max_y), 0, 255);
-                CRGB color = ColorFromPalette(lp->curr_palette, color_index, 255, lp->curr_blending);
+                CRGB color = ColorFromPalette(_lp->curr_palette, color_index, 255, _lp->curr_blending);
 
-                lp->set_xy(bar_x, bar_y, color);
+                _lp->set_xy(bar_x, bar_y, color);
             } else {
-                lp->set_xy(bar_x, bar_y, CRGB::Black);
+                _lp->set_xy(bar_x, bar_y, CRGB::Black);
             }
         }
     }
 }
 
+// Generates side-scrolling spectrogram. Based on ESP32 FFT VU code.
 void LEDWaterfallPattern::set_leds(int *intensity) {
     for (int bar_y = 0; bar_y < GRID_H; bar_y++) {
         // Draw right line
-        lp->set_xy(GRID_W - 1, bar_y, CHSV(constrain(map(intensity[bar_y], 0, 255, 160, 0), 0, 160), 255, 255));
+        _lp->set_xy(GRID_W - 1, bar_y, CHSV(constrain(map(intensity[bar_y], 0, 255, 160, 0), 0, 160), 255, 255));
 
         // Move screen left starting at 2nd row from left
         if (bar_y == GRID_H - 1) {  // do this on the last cycle
             for (int bar_x = 1; bar_x < GRID_W; bar_x++) {
                 for (int y = 0; y < GRID_H; y++) {
-                    int pixelIndexX = lp->grid_to_idx(bar_x - 1, y);
-                    int pixelIndex = lp->grid_to_idx(bar_x, y);
-                    lp->leds[pixelIndexX] = lp->leds[pixelIndex];
+                    int pixelIndexX = _lp->grid_to_idx(bar_x - 1, y);
+                    int pixelIndex = _lp->grid_to_idx(bar_x, y);
+                    _lp->leds[pixelIndexX] = _lp->leds[pixelIndex];
                 }
             }
         }
     }
 }
 
+// Generates left-right symmetric serpentine grid pattern that illuminates and fades over time.
 void LEDSymSnakeGridPattern::set_leds(int *intensity) {
     // Left half of array
     /*
@@ -248,13 +226,13 @@ void LEDSymSnakeGridPattern::set_leds(int *intensity) {
     for (int i = 0; i < GRID_H; i++) {
         if (i % 2 == 0) {
             for (int j = 0; j < GRID_W / 2; j++) {
-                lp->leds[GRID_W * i + GRID_W / 2 - 1 - j] = ColorFromPalette(lp->curr_palette, int(color_index), pgm_read_byte(&GAMMA8[int(intensity[curr_index] * 255.0 / double(BRIGHT_LEVELS))]), lp->curr_blending);
+                _lp->leds[GRID_W * i + GRID_W / 2 - 1 - j] = ColorFromPalette(_lp->curr_palette, int(color_index), pgm_read_byte(&GAMMA8[int(intensity[curr_index] * 255.0 / double(BRIGHT_LEVELS))]), _lp->curr_blending);
                 color_index += 255.0 / (NUM_LEDS / 2);
                 curr_index += 1;
             }
         } else {
             for (int j = 0; j < GRID_W / 2; j++) {
-                lp->leds[GRID_W * (i + 1) - 1 - j] = ColorFromPalette(lp->curr_palette, int(color_index), pgm_read_byte(&GAMMA8[int(intensity[curr_index] * 255.0 / double(BRIGHT_LEVELS))]), lp->curr_blending);
+                _lp->leds[GRID_W * (i + 1) - 1 - j] = ColorFromPalette(_lp->curr_palette, int(color_index), pgm_read_byte(&GAMMA8[int(intensity[curr_index] * 255.0 / double(BRIGHT_LEVELS))]), _lp->curr_blending);
                 color_index += 255.0 / (NUM_LEDS / 2);
                 curr_index += 1;
             }
@@ -278,7 +256,7 @@ void LEDSymSnakeGridPattern::set_leds(int *intensity) {
     for (int i = 0; i < GRID_H; i++) {
         if (i % 2 == 0) {
             for (int j = 0; j < GRID_W / 2; j++) {
-                lp->leds[GRID_W * i + GRID_W / 2 + j] = ColorFromPalette(lp->curr_palette, int(color_index), pgm_read_byte(&GAMMA8[int(intensity[curr_index] * 255.0 / double(BRIGHT_LEVELS))]), lp->curr_blending);
+                _lp->leds[GRID_W * i + GRID_W / 2 + j] = ColorFromPalette(_lp->curr_palette, int(color_index), pgm_read_byte(&GAMMA8[int(intensity[curr_index] * 255.0 / double(BRIGHT_LEVELS))]), _lp->curr_blending);
                 // TODO: Pull color from palette first at intensity[curr_index]; _then_ apply gamma to adjust.
                 // TODO: Use same gammas for RGB that are used on RaspPi
                 color_index += 255.0 / (NUM_LEDS / 2);
@@ -286,7 +264,7 @@ void LEDSymSnakeGridPattern::set_leds(int *intensity) {
             }
         } else {
             for (int j = 0; j < GRID_W / 2; j++) {
-                lp->leds[GRID_W * i + j] = ColorFromPalette(lp->curr_palette, int(color_index), pgm_read_byte(&GAMMA8[int(intensity[curr_index] * 255.0 / double(BRIGHT_LEVELS))]), lp->curr_blending);
+                _lp->leds[GRID_W * i + j] = ColorFromPalette(_lp->curr_palette, int(color_index), pgm_read_byte(&GAMMA8[int(intensity[curr_index] * 255.0 / double(BRIGHT_LEVELS))]), _lp->curr_blending);
                 color_index += 255.0 / (NUM_LEDS / 2);
                 curr_index += 1;
             }
@@ -294,6 +272,7 @@ void LEDSymSnakeGridPattern::set_leds(int *intensity) {
     }
 }
 
+// Generate a side-scrolling LED pattern (similar to the waterfall effect).
 void LEDScrollingPattern::set_leds(int *intensity) {
     // Generate an extra wide representation of the colors
     int effective_w = GRID_W * SCROLL_AVG_FACTOR;
@@ -306,10 +285,10 @@ void LEDScrollingPattern::set_leds(int *intensity) {
     for (int x = 0; x < effective_w; x++) {
         for (int y = 0; y < GRID_H; y++) {
             if (x == effective_w - 1) {
-                lp->colors_grid_wide[x][y] = ColorFromPalette(lp->curr_palette, int(color_index), pgm_read_byte(&GAMMA8[int(intensity[y] * 255.0 / double(BRIGHT_LEVELS))]), lp->curr_blending);
+                _lp->colors_grid_wide[x][y] = ColorFromPalette(_lp->curr_palette, int(color_index), pgm_read_byte(&GAMMA8[int(intensity[y] * 255.0 / double(BRIGHT_LEVELS))]), _lp->curr_blending);
                 color_index += 255.0 / (GRID_H);
             } else {
-                lp->colors_grid_wide[x][y] = lp->colors_grid_wide[x + 1][y];
+                _lp->colors_grid_wide[x][y] = _lp->colors_grid_wide[x + 1][y];
                 // colors_grid_wide[x][y].fadeToBlackBy(32); // note this is an exponential decay by XX/255. 32 makes this look like a standard spectrogram
             }
         }
@@ -320,12 +299,12 @@ void LEDScrollingPattern::set_leds(int *intensity) {
             avg_g = 0;
             avg_b = 0;
             for (int i = 0; i < SCROLL_AVG_FACTOR; i++) {
-                avg_r += lp->colors_grid_wide[x * SCROLL_AVG_FACTOR + i][y].red;
-                avg_g += lp->colors_grid_wide[x * SCROLL_AVG_FACTOR + i][y].green;
-                avg_b += lp->colors_grid_wide[x * SCROLL_AVG_FACTOR + i][y].blue;
+                avg_r += _lp->colors_grid_wide[x * SCROLL_AVG_FACTOR + i][y].red;
+                avg_g += _lp->colors_grid_wide[x * SCROLL_AVG_FACTOR + i][y].green;
+                avg_b += _lp->colors_grid_wide[x * SCROLL_AVG_FACTOR + i][y].blue;
             }
 
-            lp->set_xy(x, y, CRGB(avg_r / SCROLL_AVG_FACTOR, avg_g / SCROLL_AVG_FACTOR, avg_b / SCROLL_AVG_FACTOR));
+            _lp->set_xy(x, y, CRGB(avg_r / SCROLL_AVG_FACTOR, avg_g / SCROLL_AVG_FACTOR, avg_b / SCROLL_AVG_FACTOR));
         }
     }
 }
--- src/LEDAudioPattern.h
+++ src/LEDAudioPattern.h
@@ -6,92 +6,105 @@
 #include "Constants.h"
 #include "FastLED.h"
 
-// forward declaration
+// Forward declaration
 class LEDPanel;
 
-// Abstract class for LED pattern generation
+// Abstract class for LED pattern generation. Create a subclass for new LED pattern functionality.
 class LEDAudioPattern {
    public:
     LEDAudioPattern(LEDPanel *lp);
     ~LEDAudioPattern();
-    virtual void set_leds(int *intensity) = 0;
-    LEDPanel *lp;  // pointer to LED panel that will be updated
+
+    virtual void set_leds(int *intensity) = 0;  // abstract method to be implemented by all subclasses
+
+   protected:
+    LEDPanel *_lp;  // pointer to LED panel object whose pixels will be updated
 };
 
+// Simplex noise pattern
 class LEDNoisePattern : public LEDAudioPattern {
    public:
     LEDNoisePattern(LEDPanel *lp) : LEDAudioPattern(lp){};
+
     void set_leds(int *intensity) override;
 
    private:
+    // Helper methods
     void _fill_noise8();
     void _map_noise_to_leds_using_palette();
 
-    static uint8_t ihue;
+    // Static variables that will remain consistent between instantiation of the class for
+    // visual consistency when switching modes
+    static uint8_t _ihue;  // hue counter
 
-    // The 16 bit version of our coordinates
-    static uint16_t x;
-    static uint16_t y;
-    static uint16_t z;
-
-    static uint16_t target_x;
-    static uint16_t target_y;
+    // Coordinates for noise pattern
+    static uint16_t _x;
+    static uint16_t _y;
+    static uint16_t _z;
+    static uint16_t _target_x;
+    static uint16_t _target_y;
 
     // We're using the x/y dimensions to map to the x/y pixels on the matrix.  We'll
     // use the z-axis for "time".  speed determines how fast time moves forward.  Try
     // 1 for a very slow moving effect, or 60 for something that ends up looking like
     // water.
-    static uint16_t speed;  // speed is set dynamically once we've started up
+    static uint16_t _speed;  // speed is set dynamically once we've started up
 
     // Scale determines how far apart the pixels in our noise matrix are.  Try
     // changing these values around to see how it affects the motion of the display.  The
     // higher the value of scale, the more "zoomed out" the noise iwll be.  A value
     // of 1 will be so zoomed in, you'll mostly see solid colors.
-    static uint16_t scale;         // scale is set dynamically once we've started up
-    static uint16_t target_scale;  // scale is set dynamically once we've started up
+    static uint16_t _scale;  // scale is set dynamically once we've started up
+    static uint16_t _target_scale;
 
-    // This is the array that we keep our computed noise values in, declare as static to avoid jumpiness when switching modes
-    static uint8_t noise[GRID_H][GRID_W];
+    // This is the array that we keep our computed noise values in
+    static uint8_t _noise[GRID_H][GRID_W];
 };
 
+// Vertical filled bar pattern
 class LEDBarsPattern : public LEDAudioPattern {
    public:
     LEDBarsPattern(LEDPanel *lp) : LEDAudioPattern(lp){};
     void set_leds(int *intensity) override;
 
    private:
-    uint8_t peaks[GRID_W] = {0};
-    unsigned long counter = 0;
+    uint8_t _peaks[GRID_W] = {0};
+    unsigned long _counter = 0;
 };
 
+// Vertical bar peaks-only pattern
 class LEDOutrunBarsPattern : public LEDAudioPattern {
    public:
     LEDOutrunBarsPattern(LEDPanel *lp) : LEDAudioPattern(lp){};
     void set_leds(int *intensity) override;
 
    private:
-    uint8_t peaks[GRID_W] = {0};
-    unsigned long counter = 0;
+    uint8_t _peaks[GRID_W] = {0};
+    unsigned long _counter = 0;
 };
 
+// Vertical bar pattern, symmetric about center
 class LEDCenterBarsPattern : public LEDAudioPattern {
    public:
     LEDCenterBarsPattern(LEDPanel *lp) : LEDAudioPattern(lp){};
     void set_leds(int *intensity) override;
 };
 
+// Side-scrolling waterfall pattern, similar to spectrogram display
 class LEDWaterfallPattern : public LEDAudioPattern {
    public:
     LEDWaterfallPattern(LEDPanel *lp) : LEDAudioPattern(lp){};
     void set_leds(int *intensity) override;
 };
 
+// Symmetric serpentine grid pattern
 class LEDSymSnakeGridPattern : public LEDAudioPattern {
    public:
     LEDSymSnakeGridPattern(LEDPanel *lp) : LEDAudioPattern(lp){};
     void set_leds(int *intensity) override;
 };
 
+// Left-right scrolling pattern
 class LEDScrollingPattern : public LEDAudioPattern {
    public:
     LEDScrollingPattern(LEDPanel *lp) : LEDAudioPattern(lp){};
