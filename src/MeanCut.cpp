29c0734d086e284ca1baf511e2ee59171fca7864
Koji Gardiner <koji.gardiner@gmail.com>
2022-09-16 16:13:08 -0700

    MeanCut comments

--- src/MeanCut.cpp
+++ src/MeanCut.cpp
@@ -24,9 +24,6 @@ int get_longest_dim(uint16_t *rgb565_arr, uint32_t length) {
     // loop through all pixels
     for (int i = 0; i < length; i++) {
         rgb565_to_rgb888(rgb565_arr[i], rgb888_arr);  // decode current pixel into rgb888_arr
-        // Serial.println("r " + String(rgb888_arr[0]));
-        // Serial.println("g " + String(rgb888_arr[1]));
-        // Serial.println("b " + String(rgb888_arr[2]));
 
         // loop through rgb components
         for (int c = 0; c < 3; c++) {
@@ -116,7 +113,7 @@ bool hue_is_larger(rgb888_t first, rgb888_t second) {
     }
 }
 
-// Calc hue
+// Calculates hue
 uint8_t rgb888_to_hue(rgb888_t rgb888) {
     // normalize
     float r_norm = rgb888.r / 255.0;
@@ -199,7 +196,7 @@ void sort_by_channel(uint16_t *rgb565_arr, uint32_t length, uint8_t channel) {
     }
 }
 
-// Calculate the mean rgb888 value from an array of rgb565 values, and store the result in a 3-element array passed in as an arg
+// Calculates the mean rgb888 value from an array of rgb565 values, and stores the result in a 3-element array passed in as an arg
 void get_mean_color(uint16_t *rgb565_arr, uint32_t length, uint8_t *mean_rgb888_arr) {
     if (length == 0) {  // avoid a divide by zero
         return;
@@ -224,7 +221,7 @@ void get_mean_color(uint16_t *rgb565_arr, uint32_t length, uint8_t *mean_rgb888_
     }
 }
 
-// Find the closest index to the mean for the sorted longest_dim channel
+// Finds the closest index to the mean for the sorted longest_dim channel
 uint32_t get_mean_idx(uint16_t *rgb565_arr, uint32_t length, uint8_t channel) {
     if (length == 0) {  // avoid a divide by zero
         return 0;
@@ -256,7 +253,7 @@ uint32_t get_mean_idx(uint16_t *rgb565_arr, uint32_t length, uint8_t channel) {
     return idx;
 }
 
-// Prep the data before calling the recursive function that will actually compute the results
+// Prepares the data before calling the recursive function that will actually compute the results
 void mean_cut(uint16_t *rgb565_arr, uint32_t length, uint8_t depth, uint8_t *results) {
     uint32_t new_length = length_above_luma_thresh(rgb565_arr, length, LUMA_THRESH);  // sort by luma and return how many values are above the threshold
 
@@ -264,16 +261,6 @@ void mean_cut(uint16_t *rgb565_arr, uint32_t length, uint8_t depth, uint8_t *res
 
     // cast to rgb888_t struct type to enable accessing rgb elements in sorting function
     sortArray((rgb888_t *)results, 1 << depth, hue_is_larger);  // sort by increasing hue
-
-    // Serial.println("Hue values:");
-    // for (int i = 0; i < (1 << depth); i++) {
-    //     rgb888_t rgb;
-    //     rgb.r = results[i * 3];
-    //     rgb.g = results[i * 3 + 1];
-    //     rgb.b = results[i * 3 + 2];
-    //     uint8_t hue = rgb888_to_hue(rgb);
-    //     Serial.println(hue);
-    // }
 }
 
 // Recursive function that actually computes the results
@@ -282,37 +269,17 @@ void mean_cut_recursive(uint16_t *rgb565_arr, uint32_t length, uint8_t depth, ui
     if ((depth == 0) || (length == 0)) {  // if length is 0 we will get black
         // uint8_t mean_rgb888_arr[3];
         get_mean_color(rgb565_arr, length, results);
-        // Serial.println("Length: " + String(length) + ", Result:" + String(results[0]) + "," + String(results[1]) + "," + String(results[2]));
-
-        // results[0] = mean_rgb888_arr[0];
-        // results[1] = mean_rgb888_arr[1];
-        // results[2] = mean_rgb888_arr[2];
         return;
     }
 
-    // uint8_t rgb888_arr[3];
-    // for (int i = 0; i < length; i++) {
-    //     rgb565_to_rgb888(rgb565_arr[i], rgb888_arr);
-    //     Serial.print(rgb888_arr[0]); Serial.print("\t"); Serial.print(rgb888_arr[1]); Serial.print("\t"); Serial.print(rgb888_arr[2]); Serial.print("\n");
-    // }
-
     uint8_t longest_dim = get_longest_dim(rgb565_arr, length);
-    // Serial.println("longest_dim = " + String(longest_dim));
 
     sort_by_channel(rgb565_arr, length, longest_dim);
     uint32_t split_idx = get_mean_idx(rgb565_arr, length, longest_dim);
 
-    // uint8_t rgb888_arr[3];
-    // for (int i = 0; i < length; i++) {
-    //     rgb565_to_rgb888(rgb565_arr[i], rgb888_arr);
-    //     Serial.print(rgb888_arr[0]); Serial.print("\t"); Serial.print(rgb888_arr[1]); Serial.print("\t"); Serial.print(rgb888_arr[2]); Serial.print("\n");
-    // }
-
     // Determine lengths of each half of the array we are splitting into
     uint32_t length0 = split_idx;           // int(length / 2);
     uint32_t length1 = length - split_idx;  // length - length0;
-
-    // Serial.println("length0="+String(length0)+",length1="+String(length1));
 
     // Copy the contents of our original array into the new split arrays
     uint16_t rgb565_arr_0[length0];
@@ -320,16 +287,6 @@ void mean_cut_recursive(uint16_t *rgb565_arr, uint32_t length, uint8_t depth, ui
 
     uint16_t rgb565_arr_1[length1];
     memcpy(rgb565_arr_1, rgb565_arr + length0, sizeof(rgb565_arr_1));
-
-    // for (int i = 0; i < length0; i++) {
-    //     rgb565_to_rgb888(rgb565_arr_0[i], rgb888_arr);
-    //     Serial.print(rgb888_arr[0]); Serial.print("\t"); Serial.print(rgb888_arr[1]); Serial.print("\t"); Serial.print(rgb888_arr[2]); Serial.print("\n");
-    // }
-    // Serial.println();
-    // for (int i = 0; i < length1; i++) {
-    //     rgb565_to_rgb888(rgb565_arr_1[i], rgb888_arr);
-    //     Serial.print(rgb888_arr[0]); Serial.print("\t"); Serial.print(rgb888_arr[1]); Serial.print("\t"); Serial.print(rgb888_arr[2]); Serial.print("\n");
-    // }
 
     // Spawn two recursive calls to handle each "half" of the pixels
     mean_cut(rgb565_arr_0, length0, depth - 1, results);
--- src/MeanCut.h
+++ src/MeanCut.h
@@ -1,28 +1,80 @@
-#ifndef MEAN_CUT_H
-#define MEAN_CUT_H
+#ifndef _MEANCUT_H
+#define _MEANCUT_H
 
 #define LUMA_THRESH 40  // threshold to determine pixels that are bright enough to consider
 
 #include <Arduino.h>
 
-// rgb888 struct type
+// This header and its associated MeanCut.cpp file define functions that take an array
+// of pixels and find the most dominant colors using a recursive strategy similar to 
+// median cut (https://en.wikipedia.org/wiki/Median_cut). The key difference is that the
+// mean is used instead of the median to separate pixels during each iteration.
+//
+// The algorithm runs as follows:
+//      1. Sort the pixels by their luma value and ignore all pixels below a specific
+//          LUMA_THRESH (see Constants.h). This helps to ignore pixels that have little
+//          color information and would not contribute to a pleasing display.
+//      2. Find the color channel (R, G, or B) with the largest min/max difference.
+//      3. Sort the pixels by the values in the channel determined in #2.
+//      4. Find the index closest to the mean value of the channel determine #2.
+//      5. Split the pixels into two buckets at the index determined in #4.
+//      6. Recursively apply steps 2-5 on each bucket. Stop when a bucket has length 0
+//          or when we have reached the max recursion depth MEAN_CUT_DEPTH (see Constants.h).
+//          If we have reached the max recursion depth, return the average R, average G,
+//          and average B value as the dominant color for that bucket.
+//      7. Sort the resulting dominant colors by their hue. This helps provide a more pleasing
+//          color palette to be used for LED visualizations.
+//
+// Finally, note that in this implementation the input is an array of 16-bit pixels encoded
+// in RGB565 format (5 bits red, 6 bits green, 5 bits blue). This follows from the JPEG decoder
+// used in the prior process in the program, which outputs pixels in this packed format. Thus
+// there are a number of conversions that occur throughout the algorithm between RGB565 and
+// the more typical RGB888 format.
+//
+
 struct rgb888_t {
     uint8_t r;
     uint8_t g;
     uint8_t b;
 };
 
+// Entry point for the mean cut algorithm, which serves as a wrapper for the recursive function
+// mean_cut_recursive. Arguments:
+//      rgb565_arr: an array of 16-bit RGB565 pixel values
+//      length: the length of the rgb565_arr array
+//      depth: the recursion depth. 2^depth colors will be returned by the algorithm.
+//      result: an array in which the resulting dominant colors will be stored.
+//          IMPORTANT: results must have a size of at least (2 ^ depth) * 3 bytes!
+//          results will store an array of 8-bit RGB triplets (e.g. {R1, G1, B1, R2, G2, B2, R3, G3, B3, ...})
+void mean_cut(uint16_t *rgb565_arr, uint32_t length, uint8_t depth, uint8_t *results);
+
+// Computes the most dominant colors recursively. Called by mean_cut().
+void mean_cut_recursive(uint16_t *rgb565_arr, uint32_t length, uint8_t depth, uint8_t *results);
+
+// Converts an RGB888 struct to an 8-bit hue value.
 uint8_t rgb888_to_hue(rgb888_t rgb888);
+
+// Converts an array of RGB88 values to an 8-bit luma value.
 uint8_t rgb888_to_luma(uint8_t *rgb888_arr);
+
+// Converts a 16-bit RGB565 value to a corresponding array of 8-bit RGB values
 void rgb565_to_rgb888(uint16_t rgb565_val, uint8_t *rgb888_arr);
+
+// Returns the color channel with the longest dimension (i.e. largest min/max difference).
+// Takes in an RGB565 array of pixels and its length.
 int get_longest_dim(uint16_t *rgb565_arr, uint32_t length);
+
+// Comparators used by sorting functions.
 bool red_is_larger(uint16_t first, uint16_t second);
 bool green_is_larger(uint16_t first, uint16_t second);
 bool blue_is_larger(uint16_t first, uint16_t second);
 bool hue_is_larger(rgb888_t first, rgb888_t second);
-void sort_by_channel(uint16_t *rgb565_arr, uint32_t length, uint8_t channel);
-void get_mean_color(uint16_t *rgb565_arr, uint32_t length, uint8_t *mean_rgb888_arr);
-void mean_cut(uint16_t *rgb565_arr, uint32_t length, uint8_t depth, uint8_t *results);
-void mean_cut_recursive(uint16_t *rgb565_arr, uint32_t length, uint8_t depth, uint8_t *results);
 
-#endif
+// Sorts an RGB565 array of pixels along a specific color channel/dimension.
+void sort_by_channel(uint16_t *rgb565_arr, uint32_t length, uint8_t channel);
+
+// Calculates the average R, average G, average B for a given RGB565 array of pixels and
+// stores the result in the mean_rgb888_arr.
+void get_mean_color(uint16_t *rgb565_arr, uint32_t length, uint8_t *mean_rgb888_arr);
+
+#endif // _MEANCUT
